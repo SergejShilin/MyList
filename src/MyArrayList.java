de0c2dbdd9bf12c0985b73e482916efac6157de3
SergejShilin <sershil68@gmail.com>
2022-07-24 23:06:41 +0300

    add sources

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,4 @@
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class MyArrayList implements Iterable<Object> {
 //public class MyArrayList implements MyList {
@@ -181,7 +179,7 @@ public class MyArrayList implements Iterable<Object> {
         // test init
         MyArrayList lstIt = new MyArrayList();
 
-        System.out.println("Test #0 : MyArrayList#Iterator.hasNext(Object)");
+        System.out.println("Test #1.1 : MyArrayList#Iterator.hasNext(Object)");
         Iterator<Object> it0 = lstIt.iterator();
         System.out.println(it0.hasNext());
         System.out.println("Size is " + lstIt.size());
@@ -189,16 +187,16 @@ public class MyArrayList implements Iterable<Object> {
         // should be 'false'
 
 
-        System.out.println("Test #00 : MyArrayList#add(Object)");
+        System.out.println("Test #1.2 : MyArrayList#add(Object)");
         for (int i = 0; i < 20; i++) {
             lstIt.add(i);
         }
         ((MyArrayList) lstIt).printAll();
         System.out.println("");
-        // should be '1, 3, 5, 7..17, 19, null, ... null'
+        // should be '0, 1, 2, 3, 4, 5, 6, 7..17, 18, 19, null, null'
 
 
-       System.out.println("Test #000 : MyArrayList#Iterator.next(Object)");
+        System.out.println("Test #1.3 : MyArrayList#Iterator.next(Object)");
         Iterator<Object> it1 = lstIt.iterator();
         int cnt = 0;
         int Sum = 0;
@@ -217,7 +215,7 @@ public class MyArrayList implements Iterable<Object> {
         // should be '20'and 190
 
 
-        System.out.println("Test #0000 : MyArrayList#Iterator.remove(Object)");
+        System.out.println("Test #1.4 : MyArrayList#Iterator.remove(Object)");
         Iterator<Object> it2 = lstIt.iterator();
         cnt = 0;
         while (it2.hasNext()) { //
@@ -228,9 +226,10 @@ public class MyArrayList implements Iterable<Object> {
         System.out.println("");
         // should be '1, 3, 5, 7..17, 19, null, ... null'
 
+
         // NEW MyArrayList
         MyArrayList lst = new MyArrayList();
-        System.out.println("Test #1 : Created NEW MyArrayList#add(Object)");
+        System.out.println("Test #2.1 : Created NEW MyArrayList#add(Object)");
         for (int i = 0; i < 20; i++) {
             lst.add(i);
        }
@@ -241,7 +240,7 @@ public class MyArrayList implements Iterable<Object> {
         // should print [ 0..19, null, null ] because of call MyArrayList#grow 2 times
 
 
-        System.out.println("Test #2 : MyArrayList#add(int, Object)");
+        System.out.println("Test #2.2 : MyArrayList#add(int, Object)");
         for (int i = 0; i < 4; i++) {
             lst.add(10,30+i);
         }
@@ -250,14 +249,14 @@ public class MyArrayList implements Iterable<Object> {
         // should print [0..9, 33, 32, 31, 30, 11..19, null..null (x9 times)]
 
 
-        System.out.println("Test #3 : MyArrayList#set(int, Object) (replacement)");
+        System.out.println("Test #2.3 : MyArrayList#set(int, Object) (replacement)");
         lst.set(10, 42);
         ((MyArrayList) lst).printAll();
         System.out.println("");
         // should print [0..9, 42, 32, 31, 30, 11..19, null..null (x8 times)]
 
 
-        System.out.println("Test #4 : MyArrayList#set(int, Object) (outbounded)");
+        System.out.println("Test #2.4 : MyArrayList#set(int, Object) (outbounded)");
         lst.set(40, "500");
         lst.set(41, "500");
         ((MyArrayList) lst).printAll();
@@ -268,7 +267,7 @@ public class MyArrayList implements Iterable<Object> {
         // and a visible size of 42 because index #41 was the last element that we added to the list
 
 
-        System.out.println("Test #5 : MyArrayList#remove(Object) (remove 1 element)");
+        System.out.println("Test #2.5 : MyArrayList#remove(Object) (remove 1 element)");
         lst.remove("500");
         ((MyArrayList) lst).printAll();
         System.out.println("Size is " + lst.size());
@@ -276,7 +275,7 @@ public class MyArrayList implements Iterable<Object> {
         // should print [0..9, 42, 30..33, 11..19, null..null]
         // visible size should be 41 because we removed 1 elements
 
-        System.out.println("Test #6 : MyArrayList#remove(Object) (remove 1 element)");
+        System.out.println("Test #2.6 : MyArrayList#remove(Object) (remove 1 element)");
         lst.remove("500");
         ((MyArrayList) lst).printAll();
         System.out.println("Size is " + lst.size());
@@ -285,7 +284,7 @@ public class MyArrayList implements Iterable<Object> {
         // visible size should be 40 because we removed 1 elements
 
 
-        System.out.println("Test #7 : MyArrayList#remove(int) (remove 1 element by index)");
+        System.out.println("Test #2.7 : MyArrayList#remove(int) (remove 1 element by index)");
         Object r = lst.remove(10);
         System.out.println("Removed element #10: " + r.toString());
         ((MyArrayList) lst).printAll();
@@ -295,49 +294,97 @@ public class MyArrayList implements Iterable<Object> {
         // visible size should be 39 because we removed 1 element
         
 
-        System.out.println("Test #8 : MyArrayList#indexOf(Object) (absence check)");
+        System.out.println("Test #2.8 : MyArrayList#indexOf(Object) (absence check)");
         System.out.print("index of c is ");
         System.out.println(lst.indexOf("500"));
         System.out.println("");
         // should be -1
 
 
-        System.out.println("Test #9 : MyArrayList#indexOf(Object) (presence check)");
+        System.out.println("Test #2.9 : MyArrayList#indexOf(Object) (presence check)");
         System.out.print("index of 9 is ");
         System.out.println(lst.indexOf(9));
         System.out.println("");
         // should be 9
 
 
-        System.out.println("Test #10 : MyArrayList#contains(Object) (absence check)");
+        System.out.println("Test #2.10 : MyArrayList#contains(Object) (absence check)");
         System.out.print("contains of \"500\" is ");
         System.out.println(lst.contains("500"));
         System.out.println("");
         // should be 'false'
 
 
-        System.out.println("Test #11 : MyArrayList#contains(Object) (presence check)");
+        System.out.println("Test #2.11 : MyArrayList#contains(Object) (presence check)");
         System.out.print("contains of 9 is ");
         System.out.println(lst.contains(9));
         System.out.println("");
         // should be 'true'
 
 
-        System.out.println("Test #12 : MyArrayList#Size()");
+        System.out.println("Test #2.12 : MyArrayList#Size()");
         System.out.println("Size is " + lst.size());
+        System.out.println("");
         // should be 39
 
         lst.clear();
-        System.out.println("Test #12 : MyArrayList#clear()");
+        System.out.println("Test #2.13 : MyArrayList#clear()");
+        System.out.println("Size is " + lst.size());
+        System.out.println("");
+        // should be 0
+
+
+        System.out.println("Test #3.1 : Created new sequence MyArrayList#add(Object)");
+        for (int i = 0; i < 20; i++) {
+            lst.add(i);
+        }
+        System.out.println("");
+
+
+        System.out.println("Test #3.2 : MyArrayList#ListIterator.next(Object)");
+        ListIterator<Object> listIt = lst.listIterator();
+        int cnt2 = 0;
+        int Sum2 = 0;
+        System.out.print("[");
+        while (listIt.hasNext()) { //
+            Object el = listIt.next();
+            System.out.print(el + ", ");
+            cnt2++;
+            Sum2 += (int) el;
+        }
+        System.out.print("]");
+        System.out.println("");
+        System.out.println("Count is " + cnt2);
+        System.out.println("Sum is " + Sum2);
+        System.out.println("");
+        // should be '20'and 190
+
+
+        System.out.println("Test #3.3 : MyArrayList#ListIterator.remove(Object)");
+        while (listIt.hasPrevious()) { //
+            Object el = listIt.previous();
+            if ((int)el%2==0) listIt.remove();
+        }
+        ((MyArrayList) lst).printAll();
+        System.out.println("");
+        // should be '1, 3, 5, 7..17, 19, null, ... null'
+
+
+        lst.clear();
+        System.out.println("Test #3.4 : MyArrayList#clear()");
         System.out.println("Size is " + lst.size());
         // should be 0
 
+
+        /**
+         * Finish
+         */
         try {
             System.out.println(lst.get(0));
             System.out.println(lst.get(1));
-            throw new RuntimeException("Test 12 didn't pass! List should be clean AF");
+            throw new RuntimeException("Test didn't pass! List should be clean AF");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Test 12 successfully passed!");
+            System.out.println("All of Test successfully passed!");
         } finally {
         	System.out.println("\n\nEnd of the tests");
         }
@@ -375,8 +422,68 @@ public class MyArrayList implements Iterable<Object> {
                 cursor = lastRet;
                 lastRet = -1;
             } catch (IndexOutOfBoundsException ex) {
-                throw new ConcurrentModificationException();
+                System.out.println(ex);
             }
         }
+    }
+
+    public ListIterator<Object> listIterator () { return new ListItr(0); }
+
+    private class ListItr extends Itr implements ListIterator<Object> {
+
+        public ListItr(int ind) {
+            super();
+            cursor = ind;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return cursor != 0;
+        }
+
+        @Override
+        public Object previous() {
+            int i = cursor - 1;
+            if (i < 0)
+                throw new NoSuchElementException();
+            cursor = i;
+            return (Object) elements[lastRet = i];
+        }
+
+        @Override
+        public int nextIndex() {
+            return cursor;
+        }
+
+        @Override
+        public int previousIndex() {
+            return cursor - 1;
+        }
+
+        @Override
+        public void set(Object o) {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+
+            try {
+                MyArrayList.this.set(lastRet, o);
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println(ex);
+            }
+
+        }
+
+        @Override
+        public void add(Object o) {
+            try {
+                int i = cursor;
+                MyArrayList.this.add(i, o);
+                cursor = i + 1;
+                lastRet = -1;
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println(ex);
+            }
+        }
+
     }
 }
